bfb340ba5ef5357fea338fdef46e47be133416ce
Eunice <eunicepatrina0@gmail.com>
2023-08-04 15:01:30 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
-name = "Eunice Patrina J"
+NAME = "Eunice Patrina J"
 
 description = """ Enthusiastic engineering graduate seeking for an opportunity in the field of software engineering 
 to enhance my knowledge and skill while contributing to the growth of the company """
 
 st.set_page_config(layout="wide")
 
-col1, col2, col3 = st.columns(1.5, 0.5, 1.5)
+col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/profile.images")
+    st.image("images/profile.jpg")
 
 with col2:
-    st.header(NAME)
+    st.title(NAME)
     st.write(description)
